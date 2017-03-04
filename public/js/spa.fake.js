25feadbefc40bb2c8fe17949a85abc974b662f94
Atsushi Hayashi <hycats@gmail.com>
2017-03-05 05:49:57 +0900

    add expenselist

--- public/js/spa.fake.js
+++ public/js/spa.fake.js
@@ -7,8 +7,8 @@ spa.fake = (function () {
 
     getLedgerList = function () {
         return [
-            { recid: 1, sdate: "2017/01/12", expense: "食費", breakdown: "その他", product: "その他", outgo: 1000 },
-            { recid: 2, sdate: "2017/01/25", expense: "食費", breakdown: "その他", product: "その他", outgo: 15264 },
+            { recid: 1, sdate: "2017/01/12", expense:0, breakdown: "その他", product: "その他", outgo: 1000 },
+            { recid: 2, sdate: "2017/01/25", expense:0, breakdown: "その他", product: "その他", outgo: 15264 },
         ];
     };
 
