01ebad04bfc77590b4b5f54c4a8be1afd8121b38
Atsushi Hayashi <hycats@gmail.com>
2017-03-10 05:21:54 +0900

    del

--- public/js/spa.model.js
+++ public/js/spa.model.js
@@ -4,34 +4,6 @@ spa.model = (function () {
     'use strict';
 
     var configMap = {
-        expense_list: [
-            { id: 0, name: "食費", inout: -1 },
-            { id: 1, name: "酒・たばこ", inout: -1 },
-            { id: 2, name: "生活用品", inout: -1 },
-            { id: 3, name: "衣類", inout: -1 },
-            { id: 4, name: "教育・育児", inout: -1 },
-            { id: 5, name: "教養娯楽", inout: -1 },
-            { id: 6, name: "車関係", inout: -1 },
-            { id: 7, name: "交通費", inout: -1 },
-            { id: 8, name: "交際費", inout: -1 },
-            { id: 9, name: "保険", inout: -1 },
-            { id: 10, name: "社会保険", inout: -1 },
-            { id: 11, name: "住居費", inout: -1 },
-            { id: 12, name: "水道光熱", inout: -1 },
-            { id: 13, name: "通信費", inout: -1 },
-            { id: 14, name: "医療", inout: -1 },
-            { id: 15, name: "税金", inout: -1 },
-            { id: 16, name: "雑費", inout: -1 },
-            { id: 17, name: "出金", inout: -1, name2: "口座預入" },
-            { id: 18, name: "その他支出", inout: -1, editable: true },
-            { id: 19, name: "雑所得支出", inout: -1, editable: true },
-            { id: 20, name: "残高調整", inout: 0 },
-            { id: 80, name: "給与", inout: 1 },
-            { id: 81, name: "賞与", inout: 1 },
-            { id: 82, name: "入金", inout: 1, name2: "口座引出" },
-            { id: 83, name: "雑所得収入", inout: 1, editable: true },
-            { id: 84, name: "その他収入", inout: 1, editable: true }
-        ]
     },
         stateMap = {
             //account_id: 0,
